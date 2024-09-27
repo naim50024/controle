ebcdb37c2c9fa4ea37743937ff9fbd8acff0bed9
naim50024 <naimmeghdir@gmail.com>
2024-09-27 15:32:49 +0200

    test conteneur

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script pour tester le point de terminaison /chat
+curl -X POST http://localhost:8000/chat \
+-H "Content-Type: application/json" \
+-d '{"prompt":"What is a LLM?" }'
